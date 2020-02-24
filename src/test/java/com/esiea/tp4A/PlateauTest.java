9c4294d15bfd4b1830ccdbed65bee8314ce3c87d
Alexendre-lx <alexendre.obli@gmail.com>
2020-02-24 15:01:05 +0100

    test Plateau

--- src/test/java/com/esiea/tp4A/PlateauTest.java
+++ src/test/java/com/esiea/tp4A/PlateauTest.java
@@ -1,4 +1,19 @@
 package com.esiea.tp4A;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PlateauTest {
+
+    private final Plateau plateau = new Plateau();
+
+    @ParameterizedTest
+
+    void testTaille(int expectedResult) {
+        assertThat(plateau.getTaille()).isEqualTo(expectedResult);
+    }
 }
+
+
