57ac250a38292d99bc62b2e44c59879c2f129a67
shefUIEGRH <mphilippon12@gmail.com>
2020-02-24 14:06:23 +0100

    add var D in Position

--- src/main/java/com/esiea/tp4A/domain/Position.java
+++ src/main/java/com/esiea/tp4A/domain/Position.java
@@ -4,19 +4,22 @@ public interface Position {
 
     int getX();
     int getY();
+    int getD();
 
-    static Position of(int x, int y) {
-        return new FixedPosition(x, y);
+    static Position of(int x, int y, int D) {
+        return new FixedPosition(x, y, D);
     }
 
     final class FixedPosition implements Position {
 
         private final int x;
         private final int y;
+        private final int D;
 
-        public FixedPosition(int x, int y) {
+        public FixedPosition(int x, int y, int D) {
             this.x = x;
             this.y = y;
+            this.D = D;
         }
 
         @Override
@@ -27,6 +30,11 @@ public interface Position {
         @Override
         public int getY() {
             return y;
+        }
+
+        @Override
+        public int getD(){
+            return D;
         }
     }
 }
