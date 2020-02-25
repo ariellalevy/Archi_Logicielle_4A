987bafbdb34b6b23b1d4a087e5b1abd514a0b6c4
GeekyGodess <marin.cline@gmail.com>
2020-02-25 14:49:48 +0100

    Modif Direction + ajout RoverMove

---
+++ src/main/java/com/esiea/tp4A/domain/RoverMove.java
@@ -0,0 +1,28 @@
+package com.esiea.tp4A.domain;
+
+import com.esiea.tp4A.domain.MarsRover;
+import com.esiea.tp4A.domain.PlanetMap;
+import com.esiea.tp4A.domain.Position;
+
+public class RoverMove implements MarsRover {
+
+    @java.lang.Override
+    public MarsRover initialize(Position position) {
+
+    }
+
+    @java.lang.Override
+    public MarsRover updateMap(PlanetMap map) {
+        return null;
+    }
+
+    @java.lang.Override
+    public MarsRover configureLaserRange(int range) {
+        return null;
+    }
+
+    @java.lang.Override
+    public Position move(String command) {
+        return null;
+    }
+}
