cdee62402327284b44027160d59bd3fd9bd0856a
Alexendre-lx <alexendre.obli@gmail.com>
2020-02-25 14:06:53 +0100

    Ajout de la collision avec l'obstacle

---
+++ src/test/java/com/esiea/tp4A/ObstacleTest.java
@@ -0,0 +1,20 @@
+package com.esiea.tp4A;
+
+import com.esiea.tp4A.code.Obstacle;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class ObstacleTest {
+
+private final Obstacle obstacle = new Obstacle(20,36,5,10);
+    @Test
+    void testCollision(){
+        assertThat(obstacle.isOnSquare(22,45));
+        System.out.println("test Passed" + obstacle.isOnSquare(18,38));
+    }
+}
