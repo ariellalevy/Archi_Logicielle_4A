111d7000bd9df1002b67873a7479958cfab7247e
Yoan <yoan.dlvx@hotmail.fr>
2020-03-03 14:02:41 +0100

    Création Laser.java + première ébauche

--- src/main/java/com/esiea/tp4A/code/Laser.java
+++ src/main/java/com/esiea/tp4A/code/Laser.java
@@ -5,11 +5,22 @@ public class Laser {
     private int distance ; // distance maximale du laser en cm
 
     public Laser(int distance){
-            this.distance = distance;
+        this.distance = distance;
     }
 
-    public boolean TouchTarget(int distance, int TargetPosX, int TargetPosY, int RoverPosX, int RoverPosY, string Direction){
+    public boolean TouchTarget(int distance, int TargetPosX, int TargetPosY, int RoverPosX, int RoverPosY, String Direction){
 
+        Direction = Rover.getDirection();
+        switch (Direction){
+            case EAST :
+                break;
+            case WEST :
+                break;
+            case NORTH :
+                break;
+            case SOUTH :
+                break;
+        }
         if(RoverPosX - TargetPosX < distance) {
             return true;
         }
