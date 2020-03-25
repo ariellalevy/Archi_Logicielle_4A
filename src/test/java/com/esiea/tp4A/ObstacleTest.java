9aed3851f5c12f9a577ced656906df9faf370be1
Alexendre-lx <alexendre.obli@gmail.com>
2020-03-25 18:20:47 +0100

    Modification Obstacle

--- src/test/java/com/esiea/tp4A/ObstacleTest.java
+++ src/test/java/com/esiea/tp4A/ObstacleTest.java
@@ -8,16 +8,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ObstacleTest {
 
-private final Obstacle obstacle = new Obstacle(20,36,5,10);
+private final Obstacle obstacle = new Obstacle(20,36);
     @Test
-    void testCollision(){
-        assertThat(obstacle.isOnSquare(22,45));
-        System.out.println("test Passed" + obstacle.isOnSquare(18,38));
-    }
-    @Test
-    void testGetPosittion(){
-        assertThat(obstacle.getPosition().positionX == obstacle.positionX && obstacle.getPosition().positionY == obstacle.positionY);
-        System.out.println("test Passed for pos " + (obstacle.getPosition().positionX == obstacle.positionX && obstacle.getPosition().positionY == obstacle.positionY));
-
+    void testPosition(){
+        assertThat(obstacle.getPositionX() == 20 && obstacle.getPositionX() == 36);
+        System.out.println("test Passed "+"Les positions sont :"+"X : "+ obstacle.getPositionX()+"Y : "+ obstacle.getPositionX());
     }
 }
