3f2c07817f7d9d129c1ffe9879bec380dbac1c7b
Alexendre-lx <alexendre.obli@gmail.com>
2020-03-29 15:54:17 +0200

    Ajout de la branche du dimanche 29 Mars

--- src/test/java/com/esiea/tp4A/ObstacleTest.java
+++ src/test/java/com/esiea/tp4A/ObstacleTest.java
@@ -2,6 +2,7 @@ package com.esiea.tp4A;
 
 import com.esiea.tp4A.code.Obstacle;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,6 +13,6 @@ private final Obstacle obstacle = new Obstacle(20,36);
     @Test
     void testPosition(){
         assertThat(obstacle.getPositionX() == 20 && obstacle.getPositionY() == 36);
-        System.out.println("test Passed "+"Les positions sont :"+"X : "+ obstacle.getPositionX()+"Y : "+ obstacle.getPositionY());
+        System.out.println("test Passed "+"Les positions sont "+"X : "+ obstacle.getPositionX()+"\n Y : "+ obstacle.getPositionY());
     }
 }
