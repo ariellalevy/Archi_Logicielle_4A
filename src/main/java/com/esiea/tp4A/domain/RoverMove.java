70e016cabef47a0a32d43defa5569bca898e3fdb
GeekyGodess <marin.cline@gmail.com>
2020-04-03 17:09:31 +0200

    correction déplacements

--- src/main/java/com/esiea/tp4A/domain/RoverMove.java
+++ src/main/java/com/esiea/tp4A/domain/RoverMove.java
@@ -6,6 +6,9 @@ import com.esiea.tp4A.domain.Position;
 
 public class RoverMove implements MarsRover {
 
+    Position position = new PositionRover(0,0, NORTH); // position initiale
+
+
     public RoverMove(int x, int y, Direction direction){}
 
     @java.lang.Override
@@ -15,14 +18,78 @@ public class RoverMove implements MarsRover {
         }
             switch(command.charAt(0)) {
                 case 'f':
-                    return Position.of(0,1, Direction.NORTH);
+                    switch (this.position.getDirection()){
+                        case NORTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY()+1,this.position.getDirection());
+                            return this.position;
+                        case SOUTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY()-1,this.position.getDirection());
+                            return this.position;
+                        case EAST:
+                            this.position = Position.of(this.position.getX()+1, this.position.getY(),this.position.getDirection());
+                            return this.position;
+                        case WEST:
+                            this.position = Position.of(this.position.getX()-1, this.position.getY(),this.position.getDirection());
+                            return this.position;
+                        default: System.out.println("Erreur commande 'f'");
+                        break;
+                    }
 
                 case 'b':
-                    return Position.of(0,-1, Direction.NORTH);
+                    switch (this.position.getDirection()){
+                        case NORTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY()-1,this.position.getDirection());
+                            return this.position;
+                        case SOUTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY()+1,this.position.getDirection());
+                            return this.position;
+                        case EAST:
+                            this.position = Position.of(this.position.getX()-1, this.position.getY(),this.position.getDirection());
+                            return this.position;
+                        case WEST:
+                            this.position = Position.of(this.position.getX()+1, this.position.getY(),this.position.getDirection());
+                            return this.position;
+                        default: System.out.println("Erreur commande 'b'");
+                            break;
+                    }
                 case 'l':
-                    return Position.of(0,0, Direction.WEST);
+                    switch (this.position.getDirection()) {
+                        case NORTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.WEST);
+                            return this.position;
+                        case SOUTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.EAST);
+                            return this.position;
+                        case EAST:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.NORTH);
+                            return this.position;
+                        case WEST:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.SOUTH);
+                            return this.position;
+                        default:
+                            System.out.println("Erreur commande 'l'");
+                            break;
+                    }
                 case 'r':
-                    return Position.of(0,0, Direction.EAST);
+                    switch (this.position.getDirection()) {
+                        case NORTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.EAST);
+                            return this.position;
+                        case SOUTH:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.WEST);
+                            return this.position;
+                        case EAST:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.SOUTH);
+                            return this.position;
+                        case WEST:
+                            this.position = Position.of(this.position.getX(), this.position.getY(), Direction.NORTH);
+                            return this.position;
+                        default:
+                            System.out.println("Erreur commande 'r'");
+                            break;
+                    }
+
+
                 default:	System.out.println("Error!");
                     return Position.of(0,0,Direction.NORTH);
             }
