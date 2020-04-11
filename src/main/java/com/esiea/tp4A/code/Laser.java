38133567c1d7602d7878ee280e0a38400dd78672
Alexendre-lx <alexendre.obli@gmail.com>
2020-04-11 15:59:57 +0200

    Modification Laser final

--- src/main/java/com/esiea/tp4A/code/Laser.java
+++ src/main/java/com/esiea/tp4A/code/Laser.java
@@ -1,32 +1,103 @@
 package com.esiea.tp4A.code;
+import com.esiea.tp4A.domain.Direction;
 import com.esiea.tp4A.domain.MarsRover;
 import com.esiea.tp4A.domain.Position;
 import com.esiea.tp4A.code.Obstacle;
 
-public abstract class Laser implements MarsRover {
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.Set;
+
+public final class Laser {
 
     private int distance;
-    private Position position;
-    private Obstacle map;
 
-    public Laser configureLaserRange(int range){
-        this.distance = range;
-        return this;
+    public Laser (int Range){
+        configureLaserRange(Range);
     }
 
-    public Position shootLaser() {
-        Position result = null;
-        int i = 0;
-        boolean destroyed = false;
+    public boolean LaserShoot(Set<Position> map, Position position){
+        boolean firstIsTouched = false;
+        var tmpObj = obsInRange(position);
+        tmpObj = orderSet(tmpObj,position.getDirection());
+        map = orderSet(map,position.getDirection());
 
-        while (i <= this.distance && !destroyed) {
+        final Set<Position> tmpList = new HashSet<>();
 
-            if (this.map.isPositionOnMap())
-                destroyed = map.removeObstaclePosition();
-            i++;
+        for (Iterator<Position> it = map.iterator(); it.hasNext(); ) {
+            Position f = it.next();
+            for (Position pos: tmpObj) {
+                if (firstIsTouched){
+                    break;
+                }
+                if (f.getY() == pos.getY() && f.getX() == pos.getX() && !firstIsTouched){
+                    tmpList.add(f);
+                    System.out.println("Objet :" + f.getX() + " " + f.getY() + "  a été detruit" );
+                    firstIsTouched = true;
+                }
+            }
         }
+        return firstIsTouched;
+    }
 
-        return result;
+ private Set<Position> orderSet(Set<Position> set,Direction dir){
+        switch (dir){
+            case NORTH:
+            case SOUTH:
+                List<Position> numbersList = new ArrayList<Position>(set) ;        //set -> list
+                numbersList.sort(new Comparator<Position>() {
+                    @Override
+                    public int compare(Position o1, Position o2) {
+                        return o1.getY() - o2.getY() ;
+                    }
+                });
+                return new LinkedHashSet<>(numbersList);
+            case EAST:
+            case WEST:
+                List<Position> numbersList1 = new ArrayList<Position>(set) ;        //set -> list
+                numbersList1.sort(new Comparator<Position>() {
+                    @Override
+                    public int compare(Position o1, Position o2) {
+                        return o1.getX() - o2.getX() ;
+                    }
+                });
+                return new LinkedHashSet<>(numbersList1);
+                default:
+                    return set;
+        }
+    }
+
+    private Set<Position> obsInRange(Position position){
+         final Set<Position> tmpList = new HashSet<>();
+            switch (position.getDirection()){
+                case EAST:
+                    for(int i = position.getX() + 1; i<= this.distance + position.getX(); i++ ){
+                        tmpList.add(Position.of(i,position.getY(),position.getDirection()));
+                    }
+                    return tmpList;
+                case WEST:
+                    for(int i = position.getX() - 1; i >= position.getX() - this.distance ; i-- ){
+                        tmpList.add(Position.of(i,position.getY(),position.getDirection()));
+                    }
+                    return tmpList;
+                case NORTH:
+                    for(int i = position.getY() + 1; i<= this.distance + position.getY(); i++ ){
+                        tmpList.add(Position.of(position.getX(),i,position.getDirection()));
+                    }
+                    return tmpList;
+                case SOUTH:
+                    for(int i = position.getY() - 1; i >= position.getY() - this.distance ; i-- ){
+                        tmpList.add(Position.of(position.getX(),i,position.getDirection()));
+                    }
+                    return tmpList;
+                default: return tmpList;
+            }
+    }
+
+    private Laser configureLaserRange(int range){
+        this.distance = range;
+        return this;
     }
 }
 
