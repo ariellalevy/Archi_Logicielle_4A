2642c0f0b77d3135c6dfb626a0cc5135f15936e6
GeekyGodess <marin.cline@gmail.com>
2020-04-12 14:55:11 +0200

    correction erreur initialisation set Plateau

--- src/main/java/com/esiea/tp4A/code/Plateau.java
+++ src/main/java/com/esiea/tp4A/code/Plateau.java
@@ -5,10 +5,7 @@ import com.esiea.tp4A.domain.Direction;
 import com.esiea.tp4A.domain.PlanetMap;
 import com.esiea.tp4A.domain.Position;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public final class Plateau implements PlanetMap{
